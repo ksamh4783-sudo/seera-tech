587db4b7fabdd7d0d228d7dbb3cfe1902a634a81
ksamh4783-sudo <ksamh4783@gmail.com>
2026-03-06 03:16:59 +0200

    Update App.tsx
    Signed-off-by: ksamh4783-sudo <ksamh4783@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from '@/components/ui/sonner';
 import { FaWhatsapp } from 'react-icons/fa'; // إضافة أيقونة واتساب
 import Navigation from './sections/Navigation';
 import Hero from './sections/Hero';
-import Services from './sections/Services';
+import Services from './sections/Myervices';
 import About from './sections/About';
 import Stats from './sections/Stats';
 import Contact from './sections/Contact';
